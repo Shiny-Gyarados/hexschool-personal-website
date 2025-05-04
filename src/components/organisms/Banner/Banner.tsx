8b816a988f8902b7d7afae4997cac82db78a8baa
Shiny-Gyarados <hunter2015850711@gmail.com>
2025-05-04 18:32:23 +0800

    [Style] set banner about skills lh to 1.5 & remove margin bottom

--- src/components/organisms/Banner/Banner.tsx
+++ src/components/organisms/Banner/Banner.tsx
@@ -12,7 +12,7 @@ function Banner(): React.JSX.Element {
                         <h1 className="banner__about-name text-primary fw-900 text-start lh-base mb-3 text-nowrap">
                             Alyse Wang
                         </h1>
-                        <h2 className="banner__about-skills text-primary fs-3 fw-bold text-start">
+                        <h2 className="banner__about-skills text-primary fs-3 fw-bold text-start lh-base mb-0">
                             前端工程師 & 職涯諮詢師
                         </h2>
                     </div>
