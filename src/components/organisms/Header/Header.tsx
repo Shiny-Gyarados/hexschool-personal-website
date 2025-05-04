34920a53520cbac13878985cd109961823ced516
Shiny-Gyarados <hunter2015850711@gmail.com>
2025-05-04 20:43:15 +0800

    [Style] update Header component background color to bg-body

--- src/components/organisms/Header/Header.tsx
+++ src/components/organisms/Header/Header.tsx
@@ -2,7 +2,7 @@ import HeaderNav from "@/components/molecules/HeaderNav";
 
 function Header(): React.JSX.Element {
     return (
-        <header className="sticky-top d-flex justify-content-center">
+        <header className="sticky-top d-flex justify-content-center bg-body">
             <HeaderNav />
         </header>
     );
