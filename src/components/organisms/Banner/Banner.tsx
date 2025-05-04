f282b9459c30288ae43afdc4c74a5f3a8a196229
Shiny-Gyarados <hunter2015850711@gmail.com>
2025-05-04 14:24:57 +0800

    [Feat] add Banner to home page

---
+++ src/components/organisms/Banner/Banner.tsx
@@ -0,0 +1,25 @@
+import "./banner.scss";
+
+function Banner(): React.JSX.Element {
+    return (
+        <div className="container-fluid">
+            <div className="banner row">
+                <div className="col-12 col-md-6 px-0">
+                    <div className="banner__avatar w-100 h-100"></div>
+                </div>
+                <div className="col-12 col-md-6 px-0">
+                    <div className="banner__about">
+                        <h1 className="banner__about-name text-primary fw-900 text-start lh-base mb-3 text-nowrap">
+                            Alyse Wang
+                        </h1>
+                        <h2 className="banner__about-skills text-primary fs-3 fw-bold text-start">
+                            前端工程師 & 職涯諮詢師
+                        </h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Banner;
--- src/components/pages/Home/Home.tsx
+++ src/components/pages/Home/Home.tsx
@@ -1,75 +1,11 @@
-import { Fragment, useState } from "react";
+import { Fragment } from "react";
+import Banner from "@/components/organisms/Banner";
 import "./home.scss";
 
 function Home(): React.JSX.Element {
-    const [count, setCount] = useState(0);
     return (
         <Fragment>
-            <h1 className="home-title p-3">Home</h1>
-            <h1>count: {count}</h1>
-            <button
-                type="button"
-                className="btn btn-primary text-cyan-500 hover:btn-red"
-                onClick={() => setCount((pre) => pre + 1)}
-            >
-                Primary
-            </button>
-            <button type="button" className="btn btn-secondary">
-                Secondary
-            </button>
-            <button type="button" className="btn btn-success">
-                Success
-            </button>
-            <button type="button" className="btn btn-danger">
-                Danger
-            </button>
-            <button type="button" className="btn btn-warning">
-                Warning
-            </button>
-            <button type="button" className="btn btn-info">
-                Info
-            </button>
-            <button type="button" className="btn btn-light">
-                Light
-            </button>
-            <button type="button" className="btn btn-dark">
-                Dark
-            </button>
-
-            <button type="button" className="btn btn-link">
-                Link
-            </button>
-            <div id="carouselExampleControls" className="carousel slide w-50" data-bs-ride="carousel">
-                <div className="carousel-inner">
-                    <div className="carousel-item active">
-                        <img src="https://fakeimg.pl/640x350" className="d-block w-100" alt="..." />
-                    </div>
-                    <div className="carousel-item">
-                        <img src="https://fakeimg.pl/640x350" className="d-block w-100" alt="..." />
-                    </div>
-                    <div className="carousel-item">
-                        <img src="https://fakeimg.pl/640x350" className="d-block w-100" alt="..." />
-                    </div>
-                </div>
-                <button
-                    className="carousel-control-prev"
-                    type="button"
-                    data-bs-target="#carouselExampleControls"
-                    data-bs-slide="prev"
-                >
-                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span className="visually-hidden">Previous</span>
-                </button>
-                <button
-                    className="carousel-control-next"
-                    type="button"
-                    data-bs-target="#carouselExampleControls"
-                    data-bs-slide="next"
-                >
-                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span className="visually-hidden">Next</span>
-                </button>
-            </div>
+            <Banner />
         </Fragment>
     );
 }
