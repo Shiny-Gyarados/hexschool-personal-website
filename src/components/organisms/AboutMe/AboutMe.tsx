903dffe65abb50733bb49697abf3287657f91f33
Shiny-Gyarados <hunter2015850711@gmail.com>
2025-05-07 23:12:45 +0800

    [Style] set heading text color to black & paragraph to gray

--- src/components/organisms/AboutMe/AboutMe.tsx
+++ src/components/organisms/AboutMe/AboutMe.tsx
@@ -36,10 +36,10 @@ function AboutMe(): React.JSX.Element {
             <div className="about-me-card bg-white">
                 <div className="about-me-card__content">
                     <p className="about-me-card__text fw-medium">
-                        <span className="d-block d-md-inline">嗨，我是Alyse</span>
-                        <span className="d-block d-md-inline">一名深耕前端技術的工程師。</span>
+                        <span className="d-block d-md-inline text-black">嗨，我是Alyse</span>
+                        <span className="d-block d-md-inline text-black">一名深耕前端技術的工程師。</span>
                     </p>
-                    <p className="about-me-card__text fw-medium">
+                    <p className="about-me-card__text fw-medium text-black">
                         擅長 React、Vue
                         等框架，同時熱愛為轉職與新手工程師提供職涯指導。邀請你與我一起，開啟更具潛能的程式與職涯之旅！
                     </p>
