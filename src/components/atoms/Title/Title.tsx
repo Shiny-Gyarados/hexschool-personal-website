7298b047e5a0a757742f854e2bebb3aaeb4de597
Shiny-Gyarados <hunter2015850711@gmail.com>
2025-05-07 23:26:09 +0800

    [Refactor] enhance Title component to accept additional props and className for better customization

--- src/components/atoms/Title/Title.tsx
+++ src/components/atoms/Title/Title.tsx
@@ -1,11 +1,16 @@
+import clsx from "clsx";
 import "./title.scss";
 
-interface TitleProps {
+interface TitleProps extends React.HTMLAttributes<HTMLHeadingElement> {
     children: React.ReactNode;
 }
 
-function Title({ children }: TitleProps): React.JSX.Element {
-    return <h2 className="title fw-bold lh-base">{children}</h2>;
+function Title({ children, className, ...props }: TitleProps): React.JSX.Element {
+    return (
+        <h2 className={clsx("title fw-bold lh-base", className)} {...props}>
+            {children}
+        </h2>
+    );
 }
 
 export default Title;
