6a2c87e773aa8c3a01d33b0a3b471e8a284fc845
Shiny-Gyarados <hunter2015850711@gmail.com>
2025-05-07 23:27:25 +0800

    [Feature] add wrap & title for blog top picks

---
+++ src/components/organisms/BlogTopPicks/BlogTopPicks.tsx
@@ -0,0 +1,12 @@
+import Title from "@/components/atoms/Title";
+import "./blog-top-picks.scss";
+
+function BlogTopPicks(): React.JSX.Element {
+    return (
+        <section className="blog-top-picks container">
+            <Title className="mb-6">部落格精選</Title>
+        </section>
+    );
+}
+
+export default BlogTopPicks;
--- src/components/pages/Home/Home.tsx
+++ src/components/pages/Home/Home.tsx
@@ -1,6 +1,7 @@
 import Banner from "@/components/organisms/Banner";
 import AboutMe from "@/components/organisms/AboutMe";
 import ProfessionalService from "@/components/organisms/ProfessionalService";
+import BlogTopPicks from "@/components/organisms/BlogTopPicks";
 
 function Home(): React.JSX.Element {
     return (
@@ -8,6 +9,7 @@ function Home(): React.JSX.Element {
             <Banner />
             <AboutMe />
             <ProfessionalService />
+            <BlogTopPicks />
         </main>
     );
 }
