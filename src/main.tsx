acf9070905b7ecc4badd93a21b59916991736506
Shiny-Gyarados <hunter2015850711@gmail.com>
2025-05-12 23:42:18 +0800

    [Refactor] remove StrictMode wrapper from main rendering in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import { StrictMode } from "react";
+// import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { HashRouter } from "react-router-dom";
 import "./styles/index.scss";
@@ -6,9 +6,7 @@ import "bootstrap/js/src/carousel";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
-    <StrictMode>
-        <HashRouter>
-            <App />
-        </HashRouter>
-    </StrictMode>
+    <HashRouter>
+        <App />
+    </HashRouter>
 );
