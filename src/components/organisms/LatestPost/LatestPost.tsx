13df08b514bdac4e7df8eb299f5baf0fd67f76b1
Shiny-Gyarados <hunter2015850711@gmail.com>
2025-05-16 01:41:23 +0800

    [Feature] add LatestPost component and integrate it into Blog page for displaying recent posts

---
+++ src/components/organisms/LatestPost/LatestPost.tsx
@@ -0,0 +1,67 @@
+import { useQuery } from "@tanstack/react-query";
+import getPostList from "@/api/getPostList";
+import { BASE_NAME } from "@/configs/global_configs";
+import ReadMoreLink from "@/components/atoms/ReadMoreLink";
+import "./latest-post.scss";
+
+function LatestPost(): React.JSX.Element {
+    const { data, isLoading, isError, error } = useQuery({
+        queryKey: ["postList", "page=1", "limit=10", "sort=date", "order=desc"],
+        queryFn: () => getPostList({ page: "1", limit: "10", sort: "date", order: "desc" }),
+        select: (data) => data.data,
+    });
+    return (
+        <div className="latest-post container-fluid">
+            <div className="row">
+                {isLoading ? (
+                    <p>Loading...</p>
+                ) : isError ? (
+                    <p>{error.message}</p>
+                ) : (
+                    Array.isArray(data) &&
+                    data.length > 0 && (
+                        <>
+                            <div className="latest-post__image col-12 col-lg-6">
+                                <img src={`${BASE_NAME}${data[0].frontmatter.image}`} alt={data[0].frontmatter.title} />
+                            </div>
+                            <div className="col-12 col-lg-6">
+                                <div className="h-100 align-content-center">
+                                    <div className="article-card__content flex-grow-1 d-flex flex-column flex-grow-1">
+                                        <time
+                                            className="article-card__date mb-1 fw-medium"
+                                            dateTime={new Date(data[0].frontmatter.date).toISOString()}
+                                        >
+                                            {`${new Date(data[0].frontmatter.date).getFullYear()}/${new Date(data[0].frontmatter.date).getMonth() + 1}/${new Date(data[0].frontmatter.date).getDate()}`}
+                                        </time>
+                                        <ul className="article-card__tags list-unstyled d-flex gap-2">
+                                            {data[0].frontmatter.tags.map((tag) => (
+                                                <li key={tag} className="article-card__tag text-primary fs-4 fw-medium">
+                                                    #{tag}
+                                                </li>
+                                            ))}
+                                        </ul>
+                                        <h3 className="article-card__title fw-bold line-clamp-2">
+                                            {data[0].frontmatter.title}
+                                        </h3>
+                                        <p className="article-card__description line-clamp-2">
+                                            {data[0].frontmatter.description}
+                                        </p>
+                                        {/* todo: 這邊要改成動態 */}
+                                        <ReadMoreLink
+                                            href="#"
+                                            // className="article-card__read-more d-inline-block w-fit px-4 py-2 text-decoration-none"
+                                        >
+                                            閱讀內文
+                                        </ReadMoreLink>
+                                    </div>
+                                </div>
+                            </div>
+                        </>
+                    )
+                )}
+            </div>
+        </div>
+    );
+}
+
+export default LatestPost;
--- src/components/pages/Blog/Blog.tsx
+++ src/components/pages/Blog/Blog.tsx
@@ -1,9 +1,11 @@
 import BlogBanner from "@/components/organisms/BlogBanner";
+import LatestPost from "@/components/organisms/LatestPost";
 
 function Blog() {
     return (
         <main>
             <BlogBanner />
+            <LatestPost />
         </main>
     );
 }
