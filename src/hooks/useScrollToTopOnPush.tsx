3ad4f9cf7ab84a534bed583c3e33afffe510ef7e
Shiny-Gyarados <hunter2015850711@gmail.com>
2025-05-18 00:25:08 +0800

    [Update] enhance useScrollToTopOnPush hook to include search parameters for improved scroll behavior

--- src/hooks/useScrollToTopOnPush.tsx
+++ src/hooks/useScrollToTopOnPush.tsx
@@ -1,14 +1,26 @@
-import { useEffect } from "react";
-import { useNavigationType, useLocation } from "react-router-dom";
+import { useEffect, useMemo } from "react";
+import { useNavigationType, useLocation, useSearchParams } from "react-router-dom";
+
+function sortByKeys(a: [string, string], b: [string, string]) {
+    return a[0].localeCompare(b[0]);
+}
 
 function useScrollToTopOnPush() {
     const navigationType = useNavigationType();
     const { pathname } = useLocation();
+    const [searchParams] = useSearchParams();
+    const paramString = useMemo(() => {
+        const params = [...searchParams.entries()].sort(sortByKeys);
+        if (params.length === 0) {
+            return "page=1";
+        }
+        return params.map(([key, value]) => `${key}=${value}`).join("&");
+    }, [searchParams]);
     useEffect(() => {
         if (navigationType === "PUSH") {
             window.scrollTo({ top: 0, left: 0, behavior: "auto" });
         }
-    }, [navigationType, pathname]);
+    }, [navigationType, pathname, paramString]);
 }
 
 export default useScrollToTopOnPush;
