cd93aab011b0ce68aec5d0d959c9b0b636a50696
Shiny-Gyarados <hunter2015850711@gmail.com>
2025-05-18 17:17:51 +0800

    [Feature] add BlogLinks component to Post page for enhanced navigation

--- src/components/pages/Post/Post.tsx
+++ src/components/pages/Post/Post.tsx
@@ -1,9 +1,11 @@
 import PostInfo from "@/components/organisms/PostInfo";
+import BlogLinks from "@/components/organisms/BlogLinks";
 
 function Post(): React.JSX.Element {
     return (
         <main>
             <PostInfo />
+            <BlogLinks />
         </main>
     );
 }
