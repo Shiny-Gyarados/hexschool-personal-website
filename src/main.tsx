1b03c29eec25805cdfea6e91807f7ea410930c1d
Shiny-Gyarados <hunter2015850711@gmail.com>
2025-05-24 22:54:51 +0800

    [Chore] add GSAP and @gsap/react dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,20 @@
-// import { StrictMode } from "react";
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { HashRouter } from "react-router-dom";
 import "./styles/index.scss";
 import "bootstrap/js/src/carousel";
 import App from "./App.tsx";
+import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
+
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(useGSAP, ScrollTrigger);
 
 createRoot(document.getElementById("root")!).render(
-    <HashRouter>
-        <App />
-    </HashRouter>
+    <StrictMode>
+        <HashRouter>
+            <App />
+        </HashRouter>
+    </StrictMode>
 );
