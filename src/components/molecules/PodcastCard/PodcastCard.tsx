38d40746e04b959e33fae610a3b24a12309176e5
Shiny-Gyarados <hunter2015850711@gmail.com>
2025-05-25 01:11:50 +0800

    [Feature] enhance PodcastCard component with GSAP animations using useRef and useTriggerAnimation hook for improved visual effects during scroll

--- src/components/molecules/PodcastCard/PodcastCard.tsx
+++ src/components/molecules/PodcastCard/PodcastCard.tsx
@@ -1,15 +1,33 @@
+import { useRef } from "react";
 import ReadMoreLink from "@/components/atoms/ReadMoreLink";
+import useTriggerAnimation from "@/hooks/useTriggerAnimation";
+import type { UseTriggerAnimationOptions } from "@/hooks/useTriggerAnimation";
 import type { PodcastItem } from "@/components/organisms/SuccessCaseForConsulting/SuccessCaseForConsulting";
 import "./podcast-card.scss";
+
+const downToUpOptions: UseTriggerAnimationOptions = {
+    startState: {
+        opacity: 0,
+        y: 100,
+    },
+    endState: {
+        opacity: 1,
+        y: 0,
+        duration: 0.7,
+        ease: "power2.out",
+    },
+};
 
 interface PodcastCardProps {
     podcastItem: PodcastItem;
 }
 
 function PodcastCard({ podcastItem }: PodcastCardProps) {
+    const podcastCardRef = useRef<HTMLDivElement>(null);
+    useTriggerAnimation(podcastCardRef, downToUpOptions);
     return (
         <li key={podcastItem.id} className="col-12 col-md-6 col-xxl-3 mb-6">
-            <div className="podcast-card">
+            <div className="podcast-card" ref={podcastCardRef}>
                 <div className="mb-4">
                     <img src={podcastItem.avatar} alt={`podcast 頭像${podcastItem.id}`} />
                 </div>
